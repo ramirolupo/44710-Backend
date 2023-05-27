ffc43a381308b8e5af3eb718570aeb9d5f62ddfa
Ramiro Lupo <luporamiro5@gmail.com>
2023-05-27 15:52:33 -0300

    Initial Commit

--- 01-PracticaIntegradora/src/db/db.js
+++ 01-PracticaIntegradora/src/db/db.js
@@ -1,3 +1,10 @@
 import mongoose from 'mongoose';
 
 const connectionString = 'mongodb+srv://ramaalupo:admin@rlcluster.v1kncvp.mongodb.net/?retryWrites=true&w=majority';
+
+try {
+    await mongoose.connect(connectionString);
+    console.log('Conectado a la base de datos MongoDB');
+} catch (error) {
+    console.log(error);
+}
