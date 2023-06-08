3f13184e8e77f815c27104bbc72b4762551914a3
Ramiro Lupo <luporamiro5@gmail.com>
2023-06-08 16:04:25 -0300

    Segunda Pre Entrega

--- 07 - Segunda Pre Entrega/src/dao/mongodb/models/products.models.js
+++ 07 - Segunda Pre Entrega/src/dao/mongodb/models/products.models.js
@@ -1,13 +1,18 @@
 import mongoose from 'mongoose';
+import mongoosePaginate from 'mongoose-paginate-v2';
 
 const productsSchema = new mongoose.Schema({
-    name: { type: String, required: true },
+    name: { type: String, required: true, unique: true },
     description: { type: String, required: true },
     price: { type: Number, required: true },
     stock: { type: Number, required: true }
 });
 
+UserSchema.plugin(mongoosePaginate);
+
+
 export const ProductsModel = mongoose.model(
    'products',
    productsSchema 
 );
+
